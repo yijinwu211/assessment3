2b6033d2e0e6cfc2dddbab300529b2a24d3da84a
yijinwu211 <51236324+yijinwu211@users.noreply.github.com>
2019-09-02 02:46:20 +1000

    Add files via upload

---
+++ Scripts/Control.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    float fall = 0;
+    public float fallSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyControl();
+    }
+
+
+    void KeyControl()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Rotate(0, 0, -90);
+
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S) || Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3 (0,-1,0);
+
+
+            fall = Time.time;
+        }
+        }
+
+
+    }
+
+
+
+
